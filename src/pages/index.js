df19a5c8658b7457b90c30d0390c683861308169
William Bensinger <bensingerbilly@gmail.com>
2018-06-29 00:13:28 -0400

    disable bootstrap

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Link from 'gatsby-link'
-import Bootstrap from 'bootstrap'
+//import Bootstrap from 'bootstrap'
 import Hero from '../components/hero'
 import Navigation from '../components/navigation'
 import Featured from '../components/featured'
